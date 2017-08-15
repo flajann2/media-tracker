11095ede5e3356b68c492d09186b6f4c74ed100c
Fred Mitchell <fred.mitchell@gmx.de>
2017-08-15 14:24:36 +0200

    seed data for admin and user

--- db/seeds/users.seeds.rb
+++ db/seeds/users.seeds.rb
@@ -1,6 +1,23 @@
-unless User.exists?(email: 'user@example.com')
-  User.create!(email: 'user@example.com',
-               password: 'password',
-               password_confirmation: 'password')
+SAMPLE_USERS =[
+  {email: 'user@example.com',
+   password: 'password1',
+   password_confirmation: 'password1'},
+  {email: 'fred@example.com',
+   password: 'password2',
+   password_confirmation: 'password2'},
+  {email: 'sofie@example.com',
+   password: 'password3',
+   password_confirmation: 'password3'},
+  {email: 'noir@example.com',
+   password: 'password4',
+   password_confirmation: 'password4'},
+  {email: 'artemis@example.com',
+   password: 'password5',
+   password_confirmation: 'password5'}
+]
+
+SAMPLE_USERS.each do |user|
+  unless User.exists?(email: user[:email])
+    User.create!(**user)
+  end
 end
-puts "ran user seeds"
