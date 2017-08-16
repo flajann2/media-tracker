675771453bf83b4f6bbafdb98a0e2da74572fc3e
Fred Mitchell <fred.mitchell@gmx.de>
2017-08-16 14:08:09 +0200

    tests are in for medium

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,4 +1,9 @@
 class Medium < ApplicationRecord
   include PgSearch
   belongs_to :consumable, polymorphic: true
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :uri, presence: true
+  validates :media_type, presence: true
 end
--- spec/models/medium_spec.rb
+++ spec/models/medium_spec.rb
@@ -1,8 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Medium, type: :model do
-  it "is not valid without a title"
-  it "is not valid without a description"
-  it "is not valid without a uri"
-  it "is not valid without a media type"
+  before(:each) do
+    @medium =Medium.new(title: "Hello Kitty",
+                        description: "short and terse",
+                        uri: "file:/something",
+                        media_type: "film",
+                        consumable: User.new)
+  end
+
+  it "is valid with all the required fields and a user" do
+    expect(@medium).to be_valid
+  end
+
+  it "is valid with all the required fields and an Admin" do
+    expect(Medium.new(title: "Hello Kitty",
+                      description: "short and terse",
+                      uri: "file:/something",
+                      media_type: "book",
+                      consumable: Admin.new)).to be_valid
+  end
+
+  it "is not valid without a title" do
+    @medium.title = nil
+    expect(@medium).to_not be_valid
+  end
+
+  it "is not valid without a description" do
+    @medium.description = nil
+    expect(@medium).to_not be_valid
+  end
+
+  it "is not valid without a uri"  do
+    @medium.uri = nil
+    expect(@medium).to_not be_valid
+  end
+
+  it "is not valid without a media type" do
+    @medium.media_type = nil
+    expect(@medium).to_not be_valid
+  end
 end
