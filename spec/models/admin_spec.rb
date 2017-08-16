668c152096659f69f7c161eaef715ac069b26b92
Fred Mitchell <fred.mitchell@gmx.de>
2017-08-16 14:29:14 +0200

    Models are complete, more or less.

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  it "should fail with the wrong password"
+  before(:each) do
+    @admin = Admin.new(email: "test@test.com",
+                       password: "password")
+  end
+
+  it "should be valid with normal fields present" do
+    expect(@admin).to be_valid
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "should fail with the wrong password"
+  before(:each) do
+    @user = User.new(email: "test@test.com",
+                     password: "password")
+  end
+
+  it "should be valid with normal fields present" do
+    expect(@user).to be_valid
+  end
 end
