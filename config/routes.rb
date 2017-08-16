bb30ce194196f2cb1e6022def08b0f696b71b47a
Fred Mitchell <fred.mitchell@gmx.de>
2017-08-16 14:43:19 +0200

    working out the details of the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-
-  mount_devise_token_auth_for 'Admin', at: 'admin_auth'
-  as :admin do
-    # Define routes for Admin within this block.
+  namespace :api do
+    namespace :auth do
+      mount_devise_token_auth_for 'User', at: 'user'
+      mount_devise_token_auth_for 'Admin', at: 'admin'
+      as :admin do
+        # Define routes for Admin within this block.
+      end
+    end
   end
-
   #devise_for :users
   root to: "home#index"
 end
